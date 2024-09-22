6e34fc158195a01657741c42583db0209451ccbf
Dhiraj-Prajapat <prajapatdheeraj701@gmail.com>
2024-09-23 01:52:45 +0530

    update css make responsive employee list page

--- src/JavaScript/employee_list.js
+++ src/JavaScript/employee_list.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 });
             }
-        }, 2000);
+        }, 500);
     };
     renderEmployees();
 
@@ -107,3 +107,13 @@ document.addEventListener('DOMContentLoaded', function () {
         return true;
     }
 });
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
